cbf179e2a61179ea45f0e578b41c3ddc74c151a9
JMAGIC_LEARNER <MaJason93@gmail.com>
2021-08-09 23:55:32 -0700

    adding video to readme

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ If your README is long, add a table of contents to make it easy for users to fin
 # Installation
 
 Link to video demonstration how this application works:
-https://drive.google.com/file/d/13Wi25qLBlX8RQuU2vu-ZwW8qSMlAgd3g/view
+https://drive.google.com/file/d/11Y3BERMrimlQ-yLw-R6D4sezo-FF2ST9/view
               
 
 1.) Clone this repository,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -22,8 +22,19 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
   try {
-    const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category}, {model: ProductTag}], 
+    const productData = await Product.findOne(
+      {
+      where:{
+      id: req.params.id, 
+      },
+      attributes: ["id", "product_name", "price", "stock", "category_id"],
+      include: [
+        { model: Category,
+        attributes: ["id","category_name"]
+      }, 
+      {model: Tag,
+        attributes: ["id","tag_name"]
+      }], 
     });
     if (!productData) {
       res.status(404).json({message: "Invalid Product ID"});
